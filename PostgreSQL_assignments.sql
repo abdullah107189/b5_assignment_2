6c9f72be404979a8d09d22846e0283683e2a7c3d
abdullah107189 <abdullah107189@gmail.com>
2025-06-21 08:21:43 +0600

    solved 5 number problem like find sighting species

--- PostgreSQL_assignments.sql
+++ PostgreSQL_assignments.sql
@@ -7,10 +7,17 @@ CREATE TABLE rangers (
     region VARCHAR(100) NOT NULL
 );
 
-INSERT INTO rangers ("name", region) VALUES
-('Alice Green', 'Northern Hills'),
-('Bob White', 'River Delta'),
-('Carol King', 'Mountain Range');
+INSERT INTO
+    rangers ("name", region)
+VALUES (
+        'Alice Green',
+        'Northern Hills'
+    ),
+    ('Bob White', 'River Delta'),
+    (
+        'Carol King',
+        'Mountain Range'
+    );
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
@@ -19,37 +26,108 @@ CREATE TABLE species (
     discovery_date TIMESTAMP NOT NULL,
     conservation_status VARCHAR(50)
 );
-INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
-('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
-('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
-('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
-('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
 
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INT REFERENCES rangers(ranger_id),
-    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers (ranger_id),
+    species_id INT REFERENCES species (species_id),
     sighting_time TIMESTAMP NOT NULL,
     "location" TEXT NOT NULL,
     notes TEXT
 );
 
-INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
-(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
-(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
-(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
 
 SELECT * FROM rangers;
+
 SELECT * FROM species;
+
 SELECT * FROM sightings;
 
 -- problem 1
-INSERT INTO rangers("name",region) VALUES ('Derek Fox', 'Coastal Plains');
+INSERT INTO
+    rangers ("name", region)
+VALUES ('Derek Fox', 'Coastal Plains');
 
 --problem 2
-SELECT count(DISTINCT species_id) as unique_species_count  FROM species;
+SELECT count(DISTINCT species_id) as unique_species_count
+FROM species;
 
 --problem 3
-SELECT * FROM sightings
-    WHERE location ILIKE '%Pass%';
+SELECT * FROM sightings WHERE location ILIKE '%Pass%';
+
+--problem 4
+SELECT r."name", count(s.sighting_id)
+FROM rangers r
+    JOIN sightings s USING (ranger_id)
+GROUP BY
+    r.name
+ORDER BY
+    r.name ASC;
